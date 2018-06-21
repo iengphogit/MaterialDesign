31300201a5f4b068770963b26af0b19238307c1b
iengpho <ieng.pho@gaea-sys.com>
2018-06-21 18:34:57 +0700

    config openCV completed.

---
+++ opencv/src/main/cpp/native-lib.cpp
@@ -0,0 +1,5 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+{}
--- opencv/src/main/java/opencv/avaboy/com/MainActivity.java
+++ opencv/src/main/java/opencv/avaboy/com/MainActivity.java
@@ -2,12 +2,42 @@ package opencv.avaboy.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG = "mMainActivity";
+
+    static {
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG,"OpenCV not loaded");
+        }else{
+            Log.d(TAG,"OpenCV loaded");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return null;
     }
 }
