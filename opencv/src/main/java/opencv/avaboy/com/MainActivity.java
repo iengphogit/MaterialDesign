e455272f7129697b895c82619f75fc66e3cd4b0a
iengpho <ieng.pho@gaea-sys.com>
2018-06-21 18:42:48 +0700

    config cameraView

--- opencv/src/main/java/opencv/avaboy/com/MainActivity.java
+++ opencv/src/main/java/opencv/avaboy/com/MainActivity.java
@@ -3,9 +3,13 @@ package opencv.avaboy.com;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
+import android.widget.Toast;
 
 import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
@@ -20,24 +24,60 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         }
     }
 
+    CameraBridgeViewBase cameraBridgeViewBase;
+    Mat mat1, mat2, mat3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.myCameraView);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-
+        mat1 = new Mat(width, height, CvType.CV_8UC4);
+        mat2 = new Mat(width, height, CvType.CV_8UC4);
+        mat3 = new Mat(width, height, CvType.CV_8UC4);
     }
 
     @Override
     public void onCameraViewStopped() {
-
+        mat1.release();
+        mat2.release();
+        mat3.release();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return null;
+
+        mat1 = inputFrame.rgba();
+        return mat1;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(cameraBridgeViewBase != null){
+            cameraBridgeViewBase.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!OpenCVLoader.initDebug()){
+            Toast.makeText(this,"There is a problem in openCV",Toast.LENGTH_SHORT).show();
+        }else{
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
